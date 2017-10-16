f6bb6ffd376f9a1bb78500c47c6f73a2a591ceec
smartOturtle <734991706@qq.com>
2017-10-16 16:04:40 +0800

    Update Saving James Bond.cc

--- Saving James Bond.cc
+++ Saving James Bond.cc
@@ -10,7 +10,7 @@ double GetDistance(const Point& p1, const Point& p2)
 {
 	return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
 }
-int GetIndex(const Gragh& g,const Point& p)
+int GetIndex(const Gragh& g, const Point& p)
 {
 	for (int i = 0; i < g.size(); ++i)
 	{
@@ -19,17 +19,17 @@ int GetIndex(const Gragh& g,const Point& p)
 	cout << "error";
 	return -1;
 }
-void BFS(const Gragh& g,int stepSize)
+void BFS(const Gragh& g, int stepSize)
 {
 	vector<bool> visitedVertex(g.size(), false);
 	queue<int> q;
 	q.push(0);
 	while (!q.empty())
 	{
-		Point& p = g[q.front()].front();
+		const auto& p = g[q.front()].front();
 		if (max(abs(p.first), abs(p.second)) + stepSize >= 50) { cout << "Yes"; return; }
 		visitedVertex[q.front()] = true;
-		for (const auto&x : g[q.front()])if (!visitedVertex[getIndex(g, x)])q.push(getIndex(g, x));
+		for (const auto&x : g[q.front()])if (!visitedVertex[GetIndex(g, x)])q.push(GetIndex(g, x));
 		q.pop();
 	}
 	cout << "No";
@@ -38,28 +38,28 @@ int main()
 {
 	const Point origin{ 0,0 };
 	const int diameter = 15;
-	int vertexCount,stepSize;
-	cin >> vertexCount>>stepSize ;
+	int vertexCount, stepSize;
+	cin >> vertexCount >> stepSize;
 	Gragh gragh(vertexCount);
 	for (int i = 0; i < vertexCount; ++i)
 	{
 		int x, y;
 		cin >> x >> y;
 		gragh[i].push_back({ x,y });
-		for(auto& iter:gragh)
+		for (auto& iter : gragh)
 		{
-			if(iter.empty())break;
-			if(iter!=gragh[i]&&GetDistance(gragh[i].front(),iter.front())<=stepSize)
+			if (iter.empty())break;
+			if (iter != gragh[i] && GetDistance(gragh[i].front(), iter.front()) <= stepSize)
 			{
 				iter.push_back(gragh[i].front());
 				gragh[i].push_back(iter.front());
 			}
 		}
 	}
-	gragh.push_back(list<Point>(1));
+	gragh.push_back(list<Point>(1, origin));
 	for (int i = 0; i < vertexCount; ++i)
 	{
-		if(GetDistance(gragh[i].front(),{})<=stepSize+diameter)
+		if (GetDistance(gragh[i].front(), {}) <= stepSize + diameter)
 		{
 			gragh[vertexCount].push_back(gragh[i].front());
 			gragh[i].push_back(origin);
