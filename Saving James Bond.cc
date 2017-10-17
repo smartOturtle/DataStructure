b98e7af1d4c9bb5d1b4983c0a6892ae8b2fbaa76
smartOturtle <734991706@qq.com>
2017-10-17 21:17:07 +0800

    Update Saving James Bond.cc

--- Saving James Bond.cc
+++ Saving James Bond.cc
@@ -1,70 +1,84 @@
 #include <algorithm>
 #include <iostream>
-#include <list>
 #include <vector>
 #include <queue>
+#include <stack>
 using namespace std;
 using Point = pair<int, int>;
-using Gragh = vector<list<Point>>;
-double GetDistance(const Point& p1, const Point& p2)
+using Gragh = vector<vector<int>>;
+double GetDistance(Point p1, Point p2={})
 {
 	return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
 }
-int GetIndex(const Gragh& g, const Point& p)
+stack<int> bfs(const Gragh& g)
 {
-	for (int i = 0; i < g.size(); ++i)
-	{
-		if (g[i].front() == p)return i;
-	}
-	cout << "error";
-	return -1;
-}
-void BFS(const Gragh& g, int stepSize)
-{
-	vector<bool> visitedVertex(g.size(), false);
+	vector<int> dist(g.size(), -1);
+	dist[0] = 0;
 	queue<int> q;
 	q.push(0);
+	vector<int> path(g.size(), -1);
 	while (!q.empty())
 	{
-		const auto& p = g[q.front()].front();
-		if (max(abs(p.first), abs(p.second)) + stepSize >= 50) { cout << "Yes"; return; }
-		visitedVertex[q.front()] = true;
-		for (const auto&x : g[q.front()])if (!visitedVertex[GetIndex(g, x)])q.push(GetIndex(g, x));
+		for (int i=0;i<g[q.front()].size();i++)
+		{
+			if (g[q.front()][i] == 1&&dist[i]==-1)
+			{
+				dist[i] = dist[q.front()] + 1;
+				path[i] = q.front();
+				q.push(i);
+			}
+		}
 		q.pop();
 	}
-	cout << "No";
+	stack<int> s;
+	for (int i = path[g.size() - 1]; i >0; i = path[i]) { s.push(i);  }
+	return s;
 }
 int main()
 {
 	const Point origin{ 0,0 };
-	const int diameter = 15;
+	const auto diameter = 15;
 	int vertexCount, stepSize;
 	cin >> vertexCount >> stepSize;
-	Gragh gragh(vertexCount);
-	for (int i = 0; i < vertexCount; ++i)
+	vector<Point> container{origin};
+	container.reserve(vertexCount + 1);
+	Gragh gragh(vertexCount+2,vector<int>(vertexCount+2,0));
+	for (int i = 1; i < vertexCount+1; ++i)
 	{
 		int x, y;
 		cin >> x >> y;
-		gragh[i].push_back({ x,y });
-		for (auto& iter : gragh)
+		container.push_back({ x,y });
+	}
+	sort(container.begin(), container.end(), [](Point p1, Point p2) { return GetDistance(p1) < GetDistance(p2); });
+	for (int i = 1; i < container.size(); ++i)
+		for (int j = 1; j < container.size(); ++j)
+			if (i != j&&GetDistance(container[i], container[j]) <= stepSize) { gragh[i][j] = 1; gragh[j][i] = 1; }
+	for (auto i = 1; i <container.size(); ++i)
+		if (GetDistance(container[i],origin) <= diameter/2.0+stepSize)
 		{
-			if (iter.empty())break;
-			if (iter != gragh[i] && GetDistance(gragh[i].front(), iter.front()) <= stepSize)
+			gragh[0][i] = 1;
+			gragh[i][0]=1;
+		}
+	if ((stepSize + diameter/2.0) >= 50)gragh[0].back() = 1;
+	else for (int i = 1; i < container.size(); ++i)
+	{
+		if (max(abs(container[i].first), abs(container[i].second)) + stepSize >= 50)gragh[i].back() = 1;
+	}
+	if (gragh[0].back() == 1)cout << 1;
+	else
+	{
+		auto s = bfs(gragh);
+		int k = -1;
+		if (!s.empty())k = s.top();
+		if (k == -1)cout << 0;
+		else
+		{
+			cout << s.size() + 1 << endl;
+			while (!s.empty())
 			{
-				iter.push_back(gragh[i].front());
-				gragh[i].push_back(iter.front());
+				cout << container[s.top()].first << " " << container[s.top()].second << endl;
+				s.pop();
 			}
 		}
 	}
-	gragh.push_back(list<Point>(1, origin));
-	for (int i = 0; i < vertexCount; ++i)
-	{
-		if (GetDistance(gragh[i].front(), {}) <= stepSize + diameter)
-		{
-			gragh[vertexCount].push_back(gragh[i].front());
-			gragh[i].push_back(origin);
-		}
-	}
-	gragh[vertexCount].swap(gragh[0]);
-	BFS(gragh, stepSize);
 }
