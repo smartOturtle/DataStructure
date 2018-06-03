234f25ba1d92cef433e46c0dcda7688efe972d84
wumk <734991706@qq.com>
2018-06-04 00:16:51 +0800

    Update PAT Judge .cpp

--- PAT Judge .cpp
+++ PAT Judge .cpp
@@ -1,88 +1,105 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 #include <algorithm>
-#include <functional>
+#include <vector>
+#include <string>
+#include <unordered_map>
 using namespace std;
-vector<int> proList;
+struct Test
+{
+    vector<int> perfectScore;
+    explicit Test(vector<int> perfectScores):perfectScore(perfectScores){}
+    vector<int> GetProblems() const { return vector<int>(perfectScore.size(),-1); }
+    bool IsPerfect(int problemId, int score)const { return perfectScore[problemId] == score; }
+};
 struct User
 {
-	int perfectNum{};
-	int id;
-	vector<int> problems;
-	int score{};
-	bool hasPassed{};
-	User(int id_,int size):id(id_),problems(size,-2){}
-	bool operator >(User const& one)const
-	{
-		if (score > one.score)return true;
-		if(score==one.score)
-		{
-			if (perfectNum > one.perfectNum)return true;
-			if (perfectNum == one.perfectNum)return id < one.id;
-		}
-		return false;
-	}
-	void Print(int rank)const
-	{
-		cout << rank << " ";
-		printf("%05d %d", id + 1,score);
-		for(auto partScore:problems)
-		{
-			if (partScore == -2)cout << " -";
-			else if (partScore == -1)cout << " 0";
-			else { cout << " " << partScore; }
-		}
-		cout << "\n";
-	}
-	void Submit(int proId,int partScore)
-	{
-		auto& thisProblem = problems[proId - 1];
-		if (partScore >= 0)hasPassed = true;
-		if(thisProblem<partScore)
-		{
-			if (thisProblem > 0)score += (partScore - thisProblem);
-			else if(partScore>0){ score += partScore; }
-			if (partScore == proList[proId - 1])perfectNum++;
-			thisProblem = partScore;
-		}
-	}
+    Test* test;
+    int userId;
+    int perfectSize = 0;
+    int totalScore = 0;
+    bool hasPassed = false;
+    vector<int> problems;
+    User(Test* test, int userId) :test(test), userId(userId), problems(test->GetProblems()) {}
 };
-vector<User> users;
-void SortAndPrint()
+struct RankList
 {
-	sort(users.begin(), users.end(), greater<User>());
-	int rank = 1,offset=0,currentScore=users[0].score;
-	for(auto& user:users)
-	{
-		if(!user.hasPassed)continue;
-		if(user.score!=currentScore)
-		{
-			rank += offset;
-			offset = 0;
-			currentScore = user.score;
-		}
-		offset++;
-		user.Print(rank);
-	}
-}
-int main()
+    vector<User> users;
+    unordered_map<int, int> userIdToIdx;
+    Test test;
+    explicit RankList(vector<int> perfectScores):test(move(perfectScores)){}
+    int GetInnerIdx(int userId)
+    {
+        if(userIdToIdx.find(userId)==userIdToIdx.end())
+        {
+            users.emplace_back(&test, userId);
+            userIdToIdx[userId] = users.size() - 1;
+        }
+        return userIdToIdx[userId];
+    }
+    void Update(int userId,int problemId,int score)
+    {
+        auto& user = users[GetInnerIdx(userId)];
+        user.problems[problemId] = max(0, user.problems[problemId]);
+        if (score >= 0)user.hasPassed = true;
+        if (score > user.problems[problemId])
+        {
+            user.totalScore += score - user.problems[problemId];
+            user.problems[problemId] = score;
+            if (test.IsPerfect(problemId, score))user.perfectSize++;
+        }
+    }
+
+    static bool GreaterThan(const User& lhs,const User& rhs)
+    {
+        if(lhs.totalScore==rhs.totalScore)
+        {
+            if (lhs.perfectSize == rhs.perfectSize)return lhs.userId < rhs.userId;
+            return lhs.perfectSize > rhs.perfectSize;
+        }
+        return lhs.totalScore > rhs.totalScore;
+    }
+    void Print()
+    {
+        sort(users.begin(), users.end(), &GreaterThan);
+        int rank = 1;
+        int preScore = -1;
+        int sameSize=0;
+        for (auto& user : users)
+        {
+            if(!user.hasPassed)continue;
+            if(user.totalScore!=preScore)
+            {
+                rank += sameSize;
+                sameSize = 1;
+                preScore = user.totalScore;
+            }
+            else sameSize++;
+            printf("%d %05d %d", rank, user.userId,user.totalScore);
+            for (auto score : user.problems)
+            {
+                cout << ' ';
+                if (score == -1)cout << '-';
+                else { cout << score; }
+            }
+            cout << '\n';
+        }
+    }
+};
+
+int main(int argc, char* argv[])
 {
-	int userSize,submSize,proSize;
-	cin >>userSize >>proSize>>submSize;
-	for (int i = 0; i < proSize; ++i)
-	{
-		int score;
-		cin >> score;
-		proList.push_back(score);
-	}
-	users.reserve(userSize);
-	for (int i = 0; i < userSize; ++i)
-		users.push_back({ i,proSize });
-	for (int i = 0; i < submSize; ++i)
-	{
-		int id, proId, partScore;
-		cin >> id >> proId >> partScore;
-		users[id - 1].Submit(proId, partScore);
-	}
-	SortAndPrint();
+    int userSize;
+    int problemSize;
+    int commitSize;
+    cin >> userSize >> problemSize >> commitSize;
+    vector<int> perfectScores(problemSize);
+    for (auto& value : perfectScores)cin >> value;
+    RankList rankList(perfectScores);
+    for (int i = 0; i < commitSize; ++i)
+    {
+        int userId, problemId, score;
+        cin >> userId >> problemId >> score;
+        rankList.Update(userId, problemId - 1, score);
+    }
+    rankList.Print();
 }
